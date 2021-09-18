316800a28ac389295eb76ca00d74a59ec3440b45
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2021-09-18 21:09:03 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 
     app = Client(
         "master",
-        bot_token=Config.BOT_TOKEN,
+        bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
         plugins=plugins,
