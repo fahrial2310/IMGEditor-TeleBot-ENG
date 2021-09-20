49eaac16308c0c416b97bc659fed4de2ba7a5c41
Alvin Junior <88810688+fahrial2310@users.noreply.github.com>
2021-09-20 19:46:23 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,10 @@ import os
 
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+        
+    BOT_NAME = os.environ.get("BOT_NAME", "")
+    
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
 
     APP_ID = int(os.environ.get("APP_ID", 12345))
 
@@ -11,6 +15,10 @@ class Config(object):
     SUPPORT_GRP = os.environ.get("SUPPORT_GRP", "")
     
     UPDATES_CH = os.environ.get("UPDATES_CH", "")
-
+    
+    SUPPORT_NAME = os.environ.get("SUPPORT_NAME", "")
+    
+    UPDATES_NAME = os.environ.get("UPDATES_NAME", "")
+    
     # Get this api from https://www.remove.bg/b/background-removal-api
     RemoveBG_API = os.environ.get("RemoveBG_API", "")
